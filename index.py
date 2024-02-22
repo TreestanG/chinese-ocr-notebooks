5eb4593b7c65ae25ddb4d90614008f131d151755
Tristan <tristangee@gmail.com>
2024-02-21 22:45:52 -0800

    add text and font style formatting, prompt user for slide number

--- index.py
+++ index.py
@@ -11,57 +11,86 @@ import pandas as pd
 
 # Copy and paste vocab here from online pdf.
 # Leave first line as is.
-
 vocab = """ 
-1. 那么 nàme pr (indicating degree) so, such
-2. 好玩儿 hǎowánr adj fun, amusing, interesting
-3. 出去 chū qu vc to go out
-4. 非常 fēicháng adv very, extremely, exceedingly
-5. 糟糕 zāogāo adj in a terrible mess; how terrible
-6. 下雨 xià yǔ vo to rain
-7. 又 yòu adv again [See Grammar 5.]
-8. 面试 miànshì v/n to interview; interview
-9. 回去 huí qu vc to go back; to return
-10. 冬天 dōngtiān n winter
-11. 夏天 xiàtiān n summer
-12. 热 rè adj hot
-13. 春天 chūntiān n spring
-14. 秋天 qiūtiān n autumn; fall
-15. 舒服 shūfu adj comfortable"""
+1. 吵 chǎo v/adj to quarrel; noisy
+2. 连 lián prep even
+3. 做饭 zuò fàn vo to cook; to prepare a meal
+4. 报纸 bàozhǐ n newspaper
+5. 广告 guǎnggào n advertisement
+6. 附近 fùjìn n vicinity; neighborhood; nearby area
+7. 套 tào m (measure word for suite or set)
+8. 公寓 gōngyù n apartment
+9. 出租 chūzū v to rent out
+10. 走路 zǒu lù vo to walk
+11. 分钟 fēnzhōng n minute
+12. 卧室 wòshì n bedroom
+13. 厨房 chúfáng n kitchen
+14. 卫生间 wèishēngjiān n bathroom"""
 
 cc = OpenCC('s2t')
 vocab = vocab.split('\n')[1:]
 
 final = []
+
+if len(vocab) > 17: 
+    print("Too many lines. Please limit total lines of string to 17.")
+    exit()
+
 for line in vocab:
     line = line.split(' ')
     if line[0].endswith('.'):
         line[0] = line[0][:-1]
+
+    
     try: 
         text = line[5:] if line[4] in ['adj', 'n', 'v', 'pr', 'adv', 'vc', 'vo'] else line[4:]
         final.append([int(line[0]), cc.convert(line[1]), " ".join(text)])
     except: 
-        text = line[4:] if line[3] in ['adj', 'n', 'v'] else line[3:]
+        text = line[4:] if line[3] in ['adj', 'n', 'v', 'pr', 'adv', 'vc', 'vo'] else line[3:]
         final.append(['', cc.convert(line[0]), " ".join(line[3:])])     
+
+print(final)
 
 SCOPES = ['https://www.googleapis.com/auth/presentations']
 
 def updateTableObject(tableId, row, column, content):
-        return {
+    return [
+        {
             "insertText": {
                 "objectId": tableId,
                 "cellLocation":{
                     "rowIndex": row,
                     "columnIndex": column
                 },
-                "text":content
+                "text": content
+            }
+        },
+        {
+            "updateTextStyle": {
+                "objectId": tableId,
+                "cellLocation": {
+                    "rowIndex": row,
+                    "columnIndex": column
+                },
+                "style": {
+                    "fontSize": {
+                        "magnitude": 9,
+                        "unit": "PT"
+                    },
+                    "fontFamily": "Calibri"
+                },
+                "textRange": {
+                    "type": "ALL"
+                },
+                "fields": "fontSize,fontFamily"
             }
         }
+    ]
 
 df = pd.read_json('config.json', orient='index')
 
 presentationId=df.at['PRESENTATION_ID', 0]
-slideNumber=int(df.at['SLIDE_NUMBER', 0])-1
+slideNumber = int(input("Which slide number? "))-1
 
 def main():
     creds = None
@@ -89,12 +118,12 @@ def main():
     
         for e in range(len(final[:8])):
             for d in range(len(final[e])):
-                requests.append(updateTableObject(tableObjectId, e, d, str(final[e][d])))
+                requests.extend(updateTableObject(tableObjectId, e, d, str(final[e][d])))
 
         for e in range(len(final[8:])):
             e += 8
             for d in range(len(final[e])):
-                requests.append(updateTableObject(secondTable, e-8, d, str(final[e][d])))
+                requests.extend(updateTableObject(secondTable, e-8, d, str(final[e][d])))
 
 
         body = {'requests': requests}
